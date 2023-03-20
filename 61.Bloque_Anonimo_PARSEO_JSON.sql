16dc2bf3332dde19e798f058db04b4e707b74fdd
Andres Cuervo <ing.andres.cuervo@gmail.com>
2023-03-20 18:38:12 -0500

    Esta es la primera prueba

---
+++ 61.Bloque_Anonimo_PARSEO_JSON.sql
@@ -0,0 +1,10 @@
+begin
+
+    agregar_articulo_json('{
+                            "nombre": "Aspiradora",
+                            "descripcion": "La aspiradora con mas potencia del mercado.",
+                            "precio": 250
+                            }');
+    
+end;
+    
